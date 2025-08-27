d16d5372173c6ddbd4f1b309c96a56dbd5feafab
himanijainattrishi <himani.jain@abes.ac.in>
2025-08-27 10:02:34 +0530

    Updated Day1 Folder

---
+++ Day-1/index.js
@@ -0,0 +1,21 @@
+// const data=require("fs")
+// data.writeFileSync("abc.txt","hello how are you")
+
+// const readData=data.readFileSync("abc.txt","utf-8")
+// console.log(readData)
+// const data=require("os")
+// console.log(data.platform())
+
+// function sum(a,b){
+//     return a+b
+// }
+// function sub(a,b)
+// {
+//     return a-b
+// }
+// module.exports={
+//     sum,sub
+// }
+const data=require("chalk")
+console.log(data.green("hello how are you"))
+console.log(data.bgBlue("hello how are you"))
